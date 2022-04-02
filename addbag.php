4caa6631f438e12137db471830661822de417d9e
ApDnoze <100912264+ApDnoze@users.noreply.github.com>
2022-04-02 10:05:38 +0200

    changement des requêtes
    J'ai changer les requêtes pour les préparer et ajouter la quantité d'article si celui-ci est déjà présent dans le panier

--- addbag.php
+++ addbag.php
@@ -1,9 +1,46 @@
 <?php
 
+include 'connection.php';
 
-$sql = "INSERT INTO `pannier`(`mail_login`, `reference`, `quantite_d_article`)
-VALUES (".$_SESSION['login'].",".$_REQUEST["ref"].",1)";
+session_start();
 
-header("location: panier.php");
+$user = $_SESSION['login'];
+$quantite = 0;
+$ref = $_REQUEST['ref'];
+
+$sql = "SELECT reference, quantite_d_article FROM pannier WHERE mail_login = '".$user."'
+AND reference = '".$ref."'";
+$table = $connection->query($sql);
+
+while ($ligne = $table->fetch()){
+
+    if ($ligne['reference'] =$ref){
+        $quantite = $ligne['quantite_d_article'] + 1;
+    }
+}
+
+if ($quantite != 0){
+    $sql = "UPDATE `pannier` SET `quantite_d_article`= :quantite
+    WHERE mail_login = ':user' AND reference = ':ref'";
+    $sql_ready = $connection->prepare($sql);
+    $sql_ready->bindParam(':user', $user, PDO::PARAM_STR);
+    $sql_ready->bindParam(':ref', $ref, PDO::PARAM_STR);
+    $sql_ready->bindParam(':quantite', $quantite, PDO::PARAM_INT);
+
+}else {
+    $quantite = 1;
+    $sql = "INSERT INTO `pannier` (`mail_login`, `reference`, `quantite_d_article`)
+    VALUES (':user', ':ref', :quantite)";
+    $sql_ready = $connection->prepare($sql);
+    $sql_ready->bindParam(':user', $user, PDO::PARAM_STR);
+    $sql_ready->bindParam(':ref', $ref, PDO::PARAM_STR);
+    $sql_ready->bindParam(':quantite', $quantite, PDO::PARAM_INT);
+}
+
+$sql_ready->execute();
+
+var_dump($sql_ready);
+
+//header("location: afficher_panier.php");
 
 ?>
