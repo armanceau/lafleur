f7bc918bd50c0845f9a94c421f5adb46838c9d25
Mathias002 <mamousset@stpbb.org>
2022-04-03 17:48:06 +0200

    Update CreationCompte.php
    ajout dans la base de données des informations de l'utilisateur

---
+++ CreationCompte.php
@@ -0,0 +1,25 @@
+<?php
+
+require "connection.php" ;
+$email=$_REQUEST['email'] ;
+$motDePasse=SHA1($_REQUEST['motDePasse']) ;
+$nom=$_REQUEST['nom'] ;
+$prenom=$_REQUEST['prenom'] ;
+$adresse=$_REQUEST['adresse'] ;
+$tel=$_REQUEST['tel'] ;
+
+$sql=$connection ->prepare('INSERT INTO utilisateur VALUES 
+("'.$email.'", "'.$motDePasse.'", "'.$nom.'", "'.$prenom.'","'.$adresse.'", '.$tel.')') ;
+$sql->bindParam('[{.2}]', $motDePasse, PDO::PARAM_INT) ;
+
+$sql->execute();
+
+echo "Vos informations ont bien été ajoutées à notre base de données ! Vous êtes maintenant inscris !"
+
+?>
+
+<html>
+    <br>
+    <br>
+   <button> <a href="Authentification.php">se connecter avec son nouveau compte </a> </button>
+</html>
