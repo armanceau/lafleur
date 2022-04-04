e4ef759fef8ad0bdecacc919c649ba2e4b03f16a
Mathias002 <mamousset@stpbb.org>
2022-04-04 17:36:44 +0200

    Create verificationAdmin.php
    vérification de l'admin

---
+++ verificationAdmin.php
@@ -0,0 +1,36 @@
+<?php 
+
+session_start() ;
+
+require "connection.php" ;
+$login=$_REQUEST['login'] ;
+$mdp=SHA1($_REQUEST['mdp']);
+
+
+$sql=$connection ->prepare('SELECT * FROM `administrateur` WHERE username_admin="'.$login.'" AND mot_de_passe_admin="'.$mdp.'"') ;
+$sql->bindParam('[{.1}]', $login, PDO::PARAM_INT) ;
+var_dump($sql);
+$sql->execute();
+
+$ligne = $sql->fetch();
+     
+if ($login==$ligne['username_admin'] && $mdp==$ligne['mot_de_passe_admin'] ) {
+    
+        $_SESSION['login'] = $login;
+        $_SESSION['mdp'] = $mdp;
+        header("Location:Gestion.php" );
+       
+}else{
+
+echo "Login ou mot de passe incorect" ;
+
+}
+?>
+
+<html>
+<br>
+<br>
+ <button> <a href="AuthentificationAdmin.php">réessayer</a> </button>
+</html>
+
+
