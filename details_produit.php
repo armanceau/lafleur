59fe71684b004333850b3198fa6a31a95573eecd
FlavienRG <flrichefeugitton@stpbb.org>
2022-04-04 16:27:42 +0200

    modifs css et ajour details produit

--- details_produit.php
+++ details_produit.php
@@ -36,7 +36,16 @@ include ("header/header.html");
                 </h2>
         </div>
         <div class="col-3">
-        <button type="button" class="btn btn-success btn-lg btn-block bouton_produit" style="border-radius: 50px;height:80px;width:400px;"><h1>15€</h1></button>
+        <button type="button" class="btn btn-success btn-lg btn-block bouton_produit" style="border-radius: 50px;height:80px;width:400px;"><p>15€</p></button>
+                <div class="commentaire text-center">
+                    <h2 class="com_titre">Commentaires</h2>
+                    <div class="commentaire_cont text-left">
+                    <img src="img/exemple.jpg" style="height: 80px;">
+                    <h3>nom</h3>
+                    <h4>poster il y a ....</h4>
+                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ab molestiae dolores consequuntur debitis quaerat quas saepe ducimus dolorem minima cumque, repellendus eaque et sunt quidem animi eligendi, odio fuga.</h3>
+                </div>
+                </div>
 
         </div>
     </div>
