46a3ff4d2e67b90ed97e2773a0ee0eab3142a516
Mathias002 <mamousset@stpbb.org>
2022-04-04 16:24:11 +0200

    l'authentification des admins

---
+++ AuthentificationAdmin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page d'identification</title>
+</head>
+
+
+<body>
+
+        <h2> S'identifiez : </h2>
+
+    <form method='GET' action='vérification.php'>
+
+        <label>login :
+        <input type="text" name="login"  placeholder="votre adresse email" required>
+        </label>
+
+</br>
+</br>
+
+        <label>mot de passe :
+        <input type="password" name="mdp" placeholder="entrez votre mot de passe" required>
+        </label>
+
+</br>
+</br>
+
+    
+
+    <input type="submit" value="s'identifier">
+
+    
+</body>
+</html>
