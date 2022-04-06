d58c6686153ffee492b29c330c9706174e53036a
MartinChndt <100912681+MartinChndt@users.noreply.github.com>
2022-04-06 16:54:02 +0200

    Update Listederoulante.php
    Form retirer et changement dans le bouton

--- Listederoulante.php
+++ Listederoulante.php
@@ -7,27 +7,26 @@
 <?php
     require 'connection.php';
 
-            $sql = 'SELECT *  FROM produit WHERE code_de_la_categorie="'.$_REQUEST["code"].'"' ;
-            $table = $connection->query($sql);
-            while ($ligne = $table->fetch()) {
-                ?>
-                    <form method="GET" action="Panier.php">
-                <?php
-                    echo "<br/><td><img src=\"IMG/".$ligne["photo"].".jpg\" alt=\"Photo Innexistante\">"."<br/>";
-                    echo "Référence: ".$ligne["reference"]."<br/>";
-                    echo "Nom: ".$ligne["designation"]."<br/>";
-                    echo "Prix: ".$ligne["prix"]."<br/>";
-                    echo "Quantité: ".$ligne["quantite_en_stock"]."<br/>";
-                ?>
-                    <button type="submit" class="btn">Ajouter au panier</button>
-                    <br/>
-        <?php
-            }
+    $sql = 'SELECT *  FROM produit WHERE code_de_la_categorie="'.$_REQUEST["code"].'"' ;
+    $table = $connection->query($sql);
+    while ($ligne = $table->fetch()) {
         ?>
+            <form method="GET" action="Panier.php">
+        <?php
+            echo "<br/><td><img src=\"IMG/".$ligne["photo"].".jpg\" alt=\"Photo Innexistante\">"."<br/>";
+            echo "Référence: ".$ligne["reference"]."<br/>";
+            echo "Nom: ".$ligne["designation"]."<br/>";
+            echo "Prix: ".$ligne["prix"]."€<br/>";
+            echo "Quantité: ".$ligne["quantite_en_stock"]."<br/>";
+        
+            echo "<a href=\"Panier.php?ref=".$ligne["reference"]." style=\"color:black;\"><button type=\"submit\" class=\"btn\">Ajouter au panier</button></a><br/>";
 
-    
-    
-</form>
+    }
+?>
+
+
+
+
 <button class="btn">Retour</button>
 
 <?php require 'footer.html'; ?>
