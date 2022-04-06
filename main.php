5b89d4b596281fbc7f1dc4ec8d83f4074f6355d9
MartinChndt <100912681+MartinChndt@users.noreply.github.com>
2022-04-06 17:27:35 +0200

    Update main.php
    Ajout 3 fleurs les plus chère à la page principal, CSS à rajouter

--- main.php
+++ main.php
@@ -17,7 +17,21 @@
 </form>
 
 
+<?php
 
+    $sql = 'SELECT * FROM produit  ORDER BY prix DESC LIMIT 3';
+    $table = $connection->query($sql);
+    while ($ligne = $table->fetch()) {
+            echo "<br/><td><img src=\"IMG/".$ligne["photo"].".jpg\" alt=\"Photo Innexistante\" height=\"200px\" width=\"200px\">"."<br/>";
+            echo "Référence: ".$ligne["reference"]."<br/>";
+            echo "Nom: ".$ligne["designation"]."<br/>";
+            echo "Prix: ".$ligne["prix"]."€<br/>";
+            echo "Quantité: ".$ligne["quantite_en_stock"]."<br/>";
+        
+            echo "<a href=\"Panier.php?ref=".$ligne["reference"]." style=\"color:black;\"><button type=\"submit\" class=\"btn\">Ajouter au panier</button></a><br/>";
+
+    }
+?>
 
 
 
