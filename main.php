119d39621a22fd9b0a6112d8661fa8fd63e00afd
MartinChndt <100912681+MartinChndt@users.noreply.github.com>
2022-04-29 08:24:10 +0200

    Update main.php

--- main.php
+++ main.php
@@ -21,21 +21,22 @@
 <h2>Nos produit du moment</h2>
 
 <?php
-    require 'connection.php';
-    $sql='SELECT * FROM produit ORDER BY prix';
+
+    $sql = 'SELECT * FROM produit  ORDER BY prix DESC LIMIT 3';
     $table = $connection->query($sql);
     while ($ligne = $table->fetch()) {
-        echo "<br/><td><img src=\"IMG/".$ligne["photo"].".jpg\" alt=\"Photo Innexistante\" width='200px' height='200px'>"."<br/>";
-        echo "Nom de la fleur: ".$ligne["designation"]."<br/>";
-        echo "Son prix: ".$ligne["prix"]."€<br/>";
+            echo "<br/><td><img src=\"IMG/".$ligne["photo"].".jpg\" alt=\"Photo Innexistante\" height=\"200px\" width=\"200px\">"."<br/>";
+            echo "Référence: ".$ligne["reference"]."<br/>";
+            echo "Nom: ".$ligne["designation"]."<br/>";
+            echo "Prix: ".$ligne["prix"]."€<br/>";
+            echo "Quantité: ".$ligne["quantite_en_stock"]."<br/>";
+        
+            echo "<a href=\"Panier.php?ref=".$ligne["reference"]." style=\"color:black;\"><button type=\"submit\" class=\"btn\">Ajouter au panier</button></a><br/>";
+
     }
 
 
 
-?>
 
-
-
-
-<?php require 'footer.html'; ?>
+require 'footer.html'; ?>
 
