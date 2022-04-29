ddc5a32853651cfe8f87dd6676cb0bdffb0d4898
armanceau2 <102350268+armanceau2@users.noreply.github.com>
2022-04-29 11:31:35 +0200

    Creation compte css

--- CreationCompte.php
+++ CreationCompte.php
@@ -1,5 +1,5 @@
 <?php
-
+include "header/header.php";
 require "connection.php" ;
 $email=$_REQUEST['email'] ;
 $motDePasse=SHA1($_REQUEST['motDePasse']) ;
@@ -14,12 +14,29 @@ $sql->bindParam('[{.2}]', $motDePasse, PDO::PARAM_INT) ;
 
 $sql->execute();
 
-echo "Vos informations ont bien été ajoutées à notre base de données ! Vous êtes maintenant inscris !"
-
 ?>
 
 <html>
-    <br>
-    <br>
-   <button> <a href="Authentification.php">se connecter avec son nouveau compte </a> </button>
-</html>
+    <body>
+        <div class="container-fluid" style = "padding-top: 230px;">
+            <div class="row">
+
+                <div class="col-2"></div>
+
+                <div class="col-8 rounded row" style = "text-align : center; background-color : #C4C4C4; padding-top : 20px; padding-bottom : 20px;">
+                    <div class="col-1"></div>
+
+                    <div class="col-10" >
+                        <p>Vos informations ont bien été ajoutées à notre base de données, vous êtes maintenant inscris !</p>
+                        <button class="rounded-pill" style = "background-color: #006430; ">
+                            <a href="Authentification.php" style = "color : white;font-family: lobster;" >Je me connecte avec mon compte</a> 
+                        </button>
+                    </div>
+
+                </div>
+
+                <div class="col-2"></div>
+
+            </div>
+        </div>
+<?php include "footer.html";?>
