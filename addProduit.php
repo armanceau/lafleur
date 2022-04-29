a47ab5014438512c7b43496833f921a32ca45384
Mathias002 <mamousset@stpbb.org>
2022-04-29 11:41:53 +0200

    Update addProduit.php

--- addProduit.php
+++ addProduit.php
@@ -11,11 +11,28 @@ $stmt->bindParam(':prixProduit', $_REQUEST["prixProduit"]);
 $stmt->bindParam(':quantite_stock_Produit', $_REQUEST["quantite_stock_Produit"]);
 $stmt->bindParam(':code_categorie_Produit', $_REQUEST["code_categorie_Produit"]);
 
-$connection->exec($sql);
+$connection->exec($stmt);
 
 
-header("location: BackOffice.php");       
+header("location: BackOffice.php");   
+
 ?>
 
 
 
+<?php
+
+
+include 'connection.php';
+
+
+
+$stmt = $connection->prepare("INSERT categorie VALUES (:codeCateg, :nomCateg)");
+$stmt->bindParam(':nomCateg', $_REQUEST["nomCategorie"]);
+$stmt->bindParam(':codeCateg', $_REQUEST["codeCategorie"]);
+
+
+$stmt->execute();
+
+header("location: BackOffice.php");       
+?>
