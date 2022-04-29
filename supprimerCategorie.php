0027a14ff158ad3728d19605250c7efa236f8d6f
MartinChndt <100912681+MartinChndt@users.noreply.github.com>
2022-04-29 11:19:57 +0200

    Update supprimerCategorie.php
    Maintenant fonctionnel

--- supprimerCategorie.php
+++ supprimerCategorie.php
@@ -1,19 +1,25 @@
 <?php
-  try {
+
     include 'connection.php';
 
-    $stmt = $connection->prepare("DELETE FROM categorie WHERE code_de_la_categorie =(:codeCateg)");
-    $stmt->bindParam(':codeCateg', $_REQUEST["codeCategorie"]);
+    try{
+        $sql = "DELETE FROM categorie WHERE code_de_la_categorie =:codeCateg";
+        echo $sql;
+        $stmt = $connection->prepare($sql);
+            $stmt->bindValue(':codeCateg', $_REQUEST["code"], PDO::PARAM_STR);
 
+            $stmt->execute();
 
-    $stmt->execute();
+            header("location: BackOffice.php");
 
-    header("location: BackOffice.php"); 
+    }catch (PDOException $e){
+        echo "Erreur: ".$e->getMessage();
+        echo"<a href =\"BackOffice.php\">Retour au BackOffice</a>";
+    }
+
+    
         
-}catch (BadFunctionCallException $e){
-    echo "connection à MySQL inpossible : ", $e->getMessage();
-    die();
-}
+
 
 
 
