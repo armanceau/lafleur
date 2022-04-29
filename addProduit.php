f07d8d75c0428fc563f68ea1e6271b3aef615ec8
Mathias002 <mamousset@stpbb.org>
2022-04-29 11:57:15 +0200

    Update addProduit.php

--- addProduit.php
+++ addProduit.php
@@ -2,16 +2,16 @@
 
 include 'connection.php';
 
-$stmt = $connection->prepare("INSERT INTO 'produit' VALUES (:referenceProduit, :designationProduit,
+$stmt = $connection->prepare("INSERT INTO produit VALUES (:referenceProduit, :designationProduit,
  :photoProduit, :prixProduit, :quantite_stock_Produit, :code_categorie_Produit)");
-$stmt->bindParam(':referenceProduit', $_REQUEST["referenceProduit"]);
-$stmt->bindParam(':designationProduit', $_REQUEST["designationProduit"]);
-$stmt->bindParam(':photoProduit', $_REQUEST["photoProduit"]);
-$stmt->bindParam(':prixProduit', $_REQUEST["prixProduit"]);
-$stmt->bindParam(':quantite_stock_Produit', $_REQUEST["quantite_stock_Produit"]);
-$stmt->bindParam(':code_categorie_Produit', $_REQUEST["code_categorie_Produit"]);
+$stmt->bindParam(':referenceProduit', $_REQUEST["referenceProduit"], PDO::PARAM_STR);
+$stmt->bindParam(':designationProduit', $_REQUEST["designationProduit"], PDO::PARAM_STR);
+$stmt->bindParam(':photoProduit', $_REQUEST["photoProduit"], PDO::PARAM_STR);
+$stmt->bindParam(':prixProduit', $_REQUEST["prixProduit"],PDO::PARAM_STR);
+$stmt->bindParam(':quantite_stock_Produit', $_REQUEST["quantite_stock_Produit"],PDO::PARAM_INT);
+$stmt->bindParam(':code_categorie_Produit', $_REQUEST["code_categorie_Produit"],PDO::PARAM_STR);
 
-$connection->exec($stmt);
+$stmt->execute();
 
 
 header("location: BackOffice.php");   
@@ -20,19 +20,3 @@ header("location: BackOffice.php");
 
 
 
-<?php
-
-
-include 'connection.php';
-
-
-
-$stmt = $connection->prepare("INSERT categorie VALUES (:codeCateg, :nomCateg)");
-$stmt->bindParam(':nomCateg', $_REQUEST["nomCategorie"]);
-$stmt->bindParam(':codeCateg', $_REQUEST["codeCategorie"]);
-
-
-$stmt->execute();
-
-header("location: BackOffice.php");       
-?>
