f71297bb5306969533e17062af4b8a2be4d03648
MartinChndt <100912681+MartinChndt@users.noreply.github.com>
2022-05-04 19:45:55 +0200

    Ajout

--- modifierProduit.php
+++ modifierProduit.php
@@ -2,9 +2,7 @@
     require 'connection.php';
 ?>
 
-<form method="get" action="modifCategorie.php">
-
-<!--slt c slt-->
+<form method="get" action="modifProduit.php">
 
 <?php
 
@@ -12,29 +10,32 @@
 $sql = 'SELECT * FROM produit WHERE reference="'.$_REQUEST["reference"].'"';
 $table = $connection->query($sql);
 while ($ligne = $table->fetch()) {
-    echo '<h2> Produit '.$ligne['designation'].'</h2>';
+    echo '<h2> Produit: '.$ligne['designation'].'</h2>';
 
     echo'<label>Référence:</label><br/>';
-    echo '<input type="text" name="newReference" value='.$ligne["reference"].' required><br/><br/>';
+    echo '<input type="text" name="newReference" value="'.$ligne["reference"].'" required><br/><br/>';
 
     echo'<label>Désignation:</label><br/>';
-    echo '<input type="text" name="newDesignation" value='.$ligne["designation"].' required><br/><br/>';
+    echo '<input type="text" name="newDesignation" value="'.$ligne["designation"].'" required><br/><br/>';
 
     echo'<label>Nom Photo:</label><br/>';
-    echo '<input type="text" name="newPhoto" value='.$ligne["photo"].'><br/><br/>';
+    echo '<input type="text" name="newPhoto" value="'.$ligne["photo"].'"><br/><br/>';
 
     echo'<label>Prix:</label><br/>';
-    echo '<input type="text" name="newPrix" value='.$ligne["prix"].' required><br/><br/>';
+    echo '<input type="text" name="newPrix" value="'.$ligne["prix"].'" required><br/><br/>';
 
     echo'<label>Quantité en stock:</label><br/>';
-    echo '<input type="text" name="newQuantite" value='.$ligne["quantite_en_stock"].' required><br/><br/>';
+    echo '<input type="text" name="newQuantite" value="'.$ligne["quantite_en_stock"].'" required><br/><br/>';
 
+    echo'<label>Code du produit actuelle: '.$ligne['code_de_la_categorie'].'</label><br/>';
+
+    echo'<select name="code">';
     $sql = 'SELECT * FROM categorie';
     $table = $connection->query($sql);
     while ($ligne = $table->fetch()) {
         echo "<option value=".$ligne['code_de_la_categorie'].">".$ligne['nom_de_la_categorie']."</option>";
     }
-
+    echo'</select><br/><br/>';
 
 
 
