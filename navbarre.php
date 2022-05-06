19af5804405224945ecb97506a0d266812e5fabb
armanceau2 <102350268+armanceau2@users.noreply.github.com>
2022-05-06 08:49:56 +0200

    navbarre sur main + rectification

--- navbarre.php
+++ navbarre.php
@@ -1,10 +1,26 @@
-<?php require 'header/header.php'; ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href='https://fonts.googleapis.com/css?family=Italianno' rel='stylesheet'>
+        <link href='css.css' rel='stylesheet'>
+        <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet'>
+        <link rel="icon" type="image/png" href="IMG\logolafleur.png">
+        <title>La Fleur</title>
+<body>
+    
+</body>
+</html>
 
 
 <body>
-    <div class="container-fluid" style = "padding-top: 30px;">
+    <div class="container-fluid" style = "padding-top: 30px; margin-top : -20px; ">
         <div class="row">
 
             <div class="col-1"></div>
@@ -27,6 +43,7 @@
                                 }
                             ?>
                         </select>
+                        <button type="submit">Rechercher</button>
                         
                     </form>
                     
