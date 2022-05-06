f1bfdfe6aabd7e56c12ed851f74e3a3e26f4ab12
armanceau2 <102350268+armanceau2@users.noreply.github.com>
2022-05-06 11:01:30 +0200

    main+liste déroulante

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ require 'navbarre.php' ?>
 
 
 
-<!-- 3 fleurs les plus chère -->
+
 
 
 
@@ -29,6 +29,7 @@ require 'navbarre.php' ?>
                 $sql = 'SELECT * FROM produit  ORDER BY prix DESC LIMIT 6';
                 $table = $connection->query($sql);
                 while ($ligne = $table->fetch()) {
+                    $ref =$ligne["reference"];
                         echo "<div class = 'col-4 border border-dark rounded' style ='padding-right : 10px; margin-right : auto; margin-left: auto; background-color : white; margin-bottom : 10px'>";
                             echo "<br/><td><img src=\"IMG/".$ligne["photo"].".jpg\" alt=\"Photo Inexistante\" height=\"180px\" width=\"290px\" style = 'border-radius : 10px;'> "."<br><br>";
                             echo "<div class ='rounded' style = 'background-color : #006430;color: white; margin-bottom : 10px;padding-bottom : 10px;'";
@@ -36,16 +37,14 @@ require 'navbarre.php' ?>
                                 echo "Nom : ".$ligne["designation"]."<br/>";
                                 echo "Prix : ".$ligne["prix"]."€ <br/>";
                                 echo "Quantité disponible : ".$ligne["quantite_en_stock"]."<br/><br>";
-                                echo "<a href=\"addbag.php?ref=".$ligne["reference"]."\" style=\"color:black;\"><button style = 'background-color : #C4C4C4;' type='submit' class=\"btn rounded\">Ajouter au panier</button></a><br/>";
-                                
+                                echo "<a href=\"addbag.php?ref=".$ligne["reference"]."\" style=\"color:black; margin-right :10px;\"><button style = 'background-color : #C4C4C4;' type='submit' class=\"btn rounded-pill\">Ajouter au panier</button></a>";
+                                echo "<a href=\"details_produit.php?categ='$ref\" style=\"color:black;\"><button style = 'background-color : #C4C4C4;' type='submit' class=\"btn rounded-pill\">Détails</button></a><br/>";
                             echo "</div>";
 
-                            
-                            
                         echo "</div>";
                 }
                 ?>
-
+    
         </div>
 
         <div class="col-1"></div>
