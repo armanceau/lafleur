f2d72f7d965c48c7fb6104d9b401c1447476b408
armanceau <100920811+armanceau@users.noreply.github.com>
2023-01-30 18:41:57 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
-<?php
+<!DOCTYPE html>
 
-header("location: main.php");
+<html lang="fr">
 
-?>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css\bootstrap-5.2.3-dist\css\bootstrap.css">
+        <link href='css\style.css' rel='stylesheet'>
+        <link rel="icon" type="image/png" href="assets\icons\logolafleur.png">
+        <title>LaFleur</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <a href="home.php">
+                        <img class="imgIndex" src="assets\icons\logoLaFleur2.PNG" alt="logo" height="" width="">
+                    </a>
+                </div>
+                <div class="col-1"></div>
+            </div>
+        </div>
+        
+    </body>
+</html>
+       
