68d35ee52a6f88c45fed4d509a333c9ef894b034
armanceau <100920811+armanceau@users.noreply.github.com>
2023-01-30 20:29:09 +0100

    update div gauche home

--- home.php
+++ home.php
@@ -1,14 +1,48 @@
 <?php 
     require 'htmlAssets\header.php'; 
-    require 'htmlAssets\navbarre.php';
 ?>
 
 
+
+
+<div class="container home">
+    <div class="row">
+        <div class="col-6">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10">
+
+                    <h2 id="homePresentation">Les <span class="spanGreen">plantes</span> représentes <br> la beauté de <br> la <span class="spanGreen">vie</span>.</h2>
+
+                    <div class="row" id="formSearchHome">
+                        <div class="col-2"></div>
+                        <div class="col-8">
+                            <form class="inputSearch" method="GET" action="ListeProduit.php">
+                                <input type="text" name="recherche" placeholder="Déja une idée ?">
+                                <button class="buttonSubmitArrow" type="submit"><img class="arrowButton" src="assets\icons\arrow.png" alt="flèche"></button>
+                            </form> 
+                        </div>
+                        <div class="col-2"></div>
+                    </div>
+
+                    
+
+                </div>
+                <div class="col-1"></div>
+            </div>
+
+
+
+            
+        </div>
+        <div class="col-6">
+
+        </div>
+    </div>
+</div>
+
 <div>
-    <form method="GET" action="ListeProduit.php">
-        <input type="text" name="recherche" placeholder="Déja une idée ?">
-        <button type="submit">-></button>
-    </form>    
+       
 </div>
 
 
