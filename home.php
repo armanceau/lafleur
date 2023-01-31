8ac9725b481809d9c7dba636160852d2b03274df
Martin Chanudet <machanudet@stpbb.org>
2023-01-31 09:21:59 +0100

    correction placement et recup id

--- home.php
+++ home.php
@@ -32,14 +32,17 @@
                 <div class="col-6">
                      <?php
                     require "connection.php";
-                        $sql=$connection->prepare("SELECT * FROM produit LIMIT 1");
+                        $sql=$connection->prepare("SELECT * FROM produit ORDER BY prix LIMIT 1");
                         $sql->execute();
                         $ligne = $sql->fetchall();
 
                         foreach($ligne as $produit){
                             $photo = $produit['photo'];
                             ?>
-                           
+                                <div>
+                                    <img src="assets/images/<?php echo $produit['photo'];?>.jpg" alt="<?php echo $produit['designation'];?>">
+                                    <a href="details_produit.php?id=<?php echo $produit['reference'];?>"><button><?php echo $produit['designation'];?> -></button></a>
+                                </div>
                             <?php
                         }
                     ?>
@@ -58,21 +61,5 @@
 </div>
 
 <?php
-require 'connection.php';
-
-    $sql= $connection->prepare("SELECT * FROM produit ORDER BY prix LIMIT 1");
-    $sql->execute();
-    $ligne = $sql->fetchall();
-
-    foreach($ligne as $produit){
-        ?>
-        <div>
-          
-          <img src="assets/images/<?php echo $produit['photo'];?>.jpg" alt="<?php echo $produit['designation'];?>">
-          <a href="details_produit.php?id="<?php echo $produit['reference'];?>><button><?php echo $produit['designation'];?> -></button></a>
-        </div>
-        <?php
-    }
-
     require 'htmlAssets\footer.html';
 ?>
