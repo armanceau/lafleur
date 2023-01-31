4c259d6ea7503db2709cda4beaba8e41f09d85af
Mathias002 <mamousset@stpbb.org>
2023-01-31 20:26:52 +0100

    rappelle code detail_produit html

--- details_produit.php
+++ details_produit.php
@@ -13,22 +13,53 @@ foreach($ligne as $produit){
     $prix = $produit["prix"];
 
 ?>
+        <div class="container_detail_produit">
 
+        <div class="left-side">
+            <div class="info_detail_produit">
+                <div class="nom_detail_produit">
+                    <p>BONSAI</p>
+                </div>
+                <div class="prix_detail_produit">
+                    <p>45 $</p>
+                </div>
+            </div>
+            
+            <div class="description_detail_produit">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius officia, voluptate repellat quas ex necessitatibus neque exercitationem obcaecati culpa incidunt cum! Accusantium aliquam delectus blanditiis ex magnam esse libero labore.</p>
+            </div>
+
+            <div class="ajout_produit_panier">
+                <div class="text_bouton">
+                    <p>Ajouter au panier</p>
+                </div>
+                <div class="bouton_ajout_panier">
+                    <a href="#"><img src="./assets/images/flèche.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="right-side">
+            <img src="./assets/images/plantes-removebg-preview 1.png"  alt="image de remplissage">
+        </div>
+
+        </div>
+        <!--
 <div>
-    <h2><?php echo $designation." ".$prix."€"; ?></h2>
+    <h2><?php //echo $designation." ".$prix."€"; ?></h2>
 </div>
 <div>
     <p>description</p>
 </div>
 
 <div>
-   <img src="assets/images/<?php echo $image;?>.jpg" alt="<?php echo $designation;?>">
+   <img src="assets/images/<?php //echo $image;?>.jpg" alt="<?php //echo $designation;?>">
 </div>
 
 <div>
-    <?php echo '<a href="addbag.php?ref='.$reference.'"> <button type="button">';?><p>Ajoutez au panier </p></button></a>
+    <?php //echo '<a href="addbag.php?ref='.$reference.'"> <button type="button">';?><p>Ajoutez au panier </p></button></a>
 </div>
-
+-->
 
 <?php 
 }
