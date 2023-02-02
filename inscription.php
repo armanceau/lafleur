aff31658ae6baa068a2d2aa08c1928cea45ae119
Mathias002 <mamousset@stpbb.org>
2023-02-02 12:00:09 +0100

    update inscription

--- inscription.php
+++ inscription.php
@@ -14,94 +14,80 @@
 
 <body>
 
-        <div class="container-fluid" style = "padding-top: 30px;">
-                <div class="row">
+        <div class="container Inscription">
 
-                        <div class="col-3"></div>
+                <div class="insInput">
+                        <div class="insLibelle">
+                                <p>NOM</p>
+                        </div>
 
-                        <div class="col-6 rounded row" style = "text-align : center; background-color : #C4C4C4; padding-top : 20px; font-family: lobster;">
+                        <div class="inscriptionInput">
+                                <input type="text" class="insInputNom">
+                        </div>  
 
-                                <div class="col-1"></div>
-                    
+                        <div class="insValidation">
+                                <img src="./assets/icons/validation.png" alt="">
+                        </div>
+                </div>
 
-                                <div class="col-10">
-                                        <h2>   Ajoutez vos informations</h2>
-                                        <form method="GET" action="CreationCompte.php">
-                                                <br>
-                                                <!-- Adresse mail (login) -->
-                                                <div class ="rounded-pill" style="font-size: 20px;font-family: lobster; color : white;background-color: #006430;">
-                                                        <label>Email:</label>
-                                                        <br>
-                                                        <div class ="rounded" style="font-size: 15px; background-color: background-color: lightgray; padding-bottom : 10px;">
-                                                                <input style ="text-align : center;" type="email" name="email" placeholder="Votre adresse mail" required ><br/>
-                                                        </div>
-                                                </div>
-                                        <br>
+                <div class="insInput">
+                        <div class="insLibelle">
+                                <p>PRENOM</p>
+                        </div>
 
-                                                <!-- Mot de passe -->
-                                                <div class ="rounded-pill" style="font-size: 20px;font-family: lobster; color : white;background-color: #006430;">
-                                                        <label>Mot de passe:</label>
-                                                        <br>
-                                                        <div class ="rounded" style="font-size: 15px; background-color: background-color: lightgray; padding-bottom : 10px;">
-                                                                <input style ="text-align : center;" type="password" name="motDePasse" maxlength="30" minlength="9" placeholder="Votre mot de passe"required><br/>
-                                                        </div>
-                                                </div>
-                                        <br>
+                        <div class="inscriptionInput">
+                                <input type="text" class="insInputPrenom">
+                        </div>
 
-                                                <!-- Nom -->
-                                                <div class ="rounded-pill" style="font-size: 20px;font-family: lobster; color : white;background-color: #006430;">
-                                                        <label>Nom:</label>
-                                                        <br>
-                                                        <div class ="rounded" style="font-size: 15px; background-color: background-color: lightgray; padding-bottom : 10px;">
-                                                                <input style ="text-align : center;" type="text" name="nom" placeholder="Votre nom" required ><br/>
-                                                        </div>
-                                                </div>
+                        <div class="insValidation">
+                                <img src="./assets/icons/validation.png" alt="">
+                        </div>
+                </div>
 
-                                        <br>
+                <div class="insInput">
+                        <div class="insLibelle">
+                                <p>ADRESSE EMAIL</p>
+                        </div>
 
-                                                <!-- Prénom -->
-                                                <div class ="rounded-pill" style="font-size: 20px;font-family: lobster; color : white;background-color: #006430;">
-                                                        <label>Prénom:</label>
-                                                        <br>
-                                                        <div class ="rounded" style="font-size: 15px; background-color: background-color: lightgray; padding-bottom : 10px;">
-                                                                <input style ="text-align : center;" type="text" name="prenom" placeholder="Votre prénom" required ><br/>
-                                                        </div>
-                                                </div>
+                        <div class="inscriptionInput">
+                                <input type="email" class="insInputEmail">
+                        </div>
 
-                                        <br>
+                        <div class="insValidation">
+                                <img src="./assets/icons/validation.png" alt="">
+                        </div>
+                </div>
 
-                                                <!-- Adresse -->
-                                                <div class ="rounded-pill" style="font-size: 20px;font-family: lobster; color : white;background-color: #006430;">
-                                                        <label>Adresse:</label>
-                                                        <br>
-                                                        <div class ="rounded" style="font-size: 15px; background-color: background-color: lightgray; padding-bottom : 10px;">
-                                                                <input style ="text-align : center;" type="text" name="adresse" placeholder="Votre adresse" required ><br/>
-                                                        </div>
-                                                </div>
-                                        <br>
+                <div class="insInput">
+                        <div class="insLibelle">
+                                <p>MOT DE PASSE</p>
+                        </div>
 
-                                                <!-- Téléphone -->
-                                                <div class ="rounded-pill" style="font-size: 20px;font-family: lobster; color : white;background-color: #006430;">
-                                                        <label>Téléphone:</label>
-                                                        <br>
-                                                        <div class ="rounded" style="font-size: 15px; background-color: background-color: lightgray; padding-bottom : 10px;">
-                                                                <input style ="text-align : center;" type="number" name="tel" maxlength="10" minlength="10" placeholder="Votre téléphone" required ><br/>
-                                                        </div>
-                                                </div>
-                                        <br>
+                        <div class="inscriptionInput">
+                                <input type="password" class="insInputMdp">
+                        </div>
 
-                                                <div style = "margin-bottom : 20px"> 
-                                                        <button type="submit">Créer</button>
-                                                        <button type="reset"> Annuler </button>
-                                                </div>  
-                                        </form>
-                                </div>
+                        <div class="insValidation">
+                                <img src="./assets/icons/validation.png" alt="">
+                        </div>
+                </div>
 
-                                <div class="col-1" ></div>
+                <div class="insInput">
+                        <div class="insLibelle">
+                                <p>ADRESSE DE LIVRAISON</p>
+                        </div>
 
-                                <div class="col-3" ></div>
+                        <div class="inscriptionInput">
+                                <input type="text" class="insInputAdresse">
+                        </div>
+
+                        <div class="insValidation">
+                                <img src="./assets/icons/validation.png" alt="">
                         </div>
                 </div>
         </div>
 
-<?php require 'htmlAssets/footer.html';?>
+
+</body>
+
+<?php require 'htmlAssets/footer2.html';?>
