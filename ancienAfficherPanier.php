5071b04d72afaefc215564fd36348e7a0d633534
MartinChndt <100912681+MartinChndt@users.noreply.github.com>
2023-02-10 16:20:52 +0100

    MAJ afficher panier
    ancien code récupéré car requête inconnue présente

--- ancienAfficherPanier.php
+++ ancienAfficherPanier.php
@@ -1,11 +1,11 @@
 <?php
 
-require 'htmlAssets\header.php'; 
+include ("htmlAssets\header.php");
 include "connection.php";
 
+
 if (!isset($_SESSION['login'])){
-    //header("location: Authentification.php");
-    echo "here";
+    header("location: Authentification.php");
 } else{
 
     $liste_produit = array();
@@ -36,7 +36,7 @@ if (!isset($_SESSION['login'])){
                         <br/>
                         <div class="row">
                             <div class="col-2-md col-3">
-                                <img class="photo_produit_panier" src="IMG/'.$ligne['photo'].'.jpg" alt="'.$ligne['photo'].'">
+                                <img class="photo_produit_panier" src="'.$ligne['photo'].'" alt="'.$ligne['photo'].'">
                             </div>
                             <div class="col-8-md col-7">
                                 <figure>
@@ -50,9 +50,9 @@ if (!isset($_SESSION['login'])){
                             <div class="col-2-md col-2">
                                 <h4 class="main_color title_prod">'.number_format($ligne['prix'], 2, ',', ' ').' €</h4>
                                 <input class="number_bag" type="text" value="'.$liste_produit[$i+1].'" style="text-align: right; border-radius: 20px;" disabled>
-                                <br/><a href="delquantite.php?ref='.$ligne['reference'].'" class="btn"><img class="quantite-control" src="header/images/moins.png"></a>
-                                <a href="addquantite.php?ref='.$ligne['reference'].'" class="btn"><img class="quantite-control" src="header/images/plus.png"></a><br/>
-                                <a class="btn" href="destroy-bag.php?ref='.$ligne['reference'].'"><img class="poubelle-panier" src="header/images/poubelle.png"></a>
+                                <br/><a href="delquantite.php?ref='.$ligne['reference'].'" class="btn"><img class="quantite-control" src="assets/icons/moins.png"></a>
+                                <a href="addquantite.php?ref='.$ligne['reference'].'" class="btn"><img class="quantite-control" src="assets/icons/plus.png"></a><br/>
+                                <a class="btn" href="destroy-bag.php?ref='.$ligne['reference'].'"><img class="poubelle-panier" src="assets/icons/poubelle.png"></a>
                             </div>
                         </div>
                         <br/>
@@ -62,15 +62,36 @@ if (!isset($_SESSION['login'])){
                     }
                     
                 }
-                    
+                
+                $pricet = $price;
+                $reduction=0;
+
+                $sql= $connection->prepare('SELECT * FROM utiliser WHERE mail_login ="'.$_SESSION["login"].'"') ;
+
+                $sql->execute();
+                $ligne = $sql->fetch();
+
+                if (!empty($ligne)){
+                    $sqlv2 = 'SELECT * FROM bon_de_reduction WHERE code_de_reduction ="'.$ligne["code_de_reduction"].'"' ;
+                    $tablev2 = $connection->query($sqlv2);
+                    $lignev2 = $tablev2->fetch();
+                    $reduction = $lignev2['valeur_de_renduction_en_pourcentage'];
+                    $reduction = $price * $reduction / 100;
+                    $pricet -= $reduction;
+                }
+                
                 echo '
                 
                 <form action="commande.php" method="post">
                     <div class="row">
-                        <div class="col-9-sm col-10">
+                        <div class="col-6-sm col-5">
+                            <label class="title_prod" for="bon">Bon de réduction :</label>
+                            <input type="text" name="bon" style="margin-left: 10px; border-radius: 5px;">
+                        </div>
+                        <div class="col-3-sm col-5">
                         </div>
                         <div class="col-3-sm col-2">
-                            <p class="title_prod">Total : <b>'.number_format($price, 2, ',', ' ').' €</b></p>
+                            <p class="title_prod">Total : <b>'.number_format($pricet, 2, ',', ' ').' € (-'.number_format($reduction, 2, ',', ' ').'€)</b></p>
                             <input type="text" name="total" value="'.$price.'" hidden>
                         </div>
                         <div class="col-9-sm col-10">
@@ -92,7 +113,9 @@ if (!isset($_SESSION['login'])){
     </main>
     ';
 
-    require 'htmlAssets\footer.html'; }
+    require 'htmlAssets\footer.html';
+
+}
 ?>
         
     </body>
